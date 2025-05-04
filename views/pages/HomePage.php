fd00b680b18f0538fc513bee00162384956db699
Vitalij <krinickij.v.a@gmail.com>
2025-05-04 18:00:09 +0300

    CHORE: eded composer to project

---
+++ views/pages/HomePage.php
@@ -0,0 +1,10 @@
+<?php
+    include __DIR__ . '/../components/header.php';
+
+?>
+ <main class="p-4 md:ml-64 h-auto pt-20"style="height:100vh">
+ <h1><?= $titleH1 ?></h1>
+ 
+</main>
+
+<?php include __DIR__ . '/../components/footer.php'; ?>
